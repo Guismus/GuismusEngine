3233dd20a47330561e1c1a7e63690267ef229724
Guismus <guillaume.boudaille@epitech.eu>
2023-10-04 02:11:20 +0200

    build: stop building share lib

---
+++ build/CMakeFiles/GuismusEngine.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GuismusEngine.dir/src/GameEngine.cpp.o"
+  "CMakeFiles/GuismusEngine.dir/src/GameEngine.cpp.o.d"
+  "libGuismusEngine.pdb"
+  "libGuismusEngine.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GuismusEngine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/GuismusEngine.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for GuismusEngine.
+# This may be replaced when dependencies are built.
